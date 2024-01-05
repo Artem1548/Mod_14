b418635fb907615731b6749f52492af1f800844e
Artem1548 <140644935+Artem1548@users.noreply.github.com>
2024-01-05 17:56:15 +0200

    Add files via upload

---
+++ src/main/java/com/example/homework/Mod_14Application.java
@@ -0,0 +1,13 @@
+package com.example.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod_14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod_14Application.class, args);
+    }
+
+}
